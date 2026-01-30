2d9b8d3a37135e7969eea4f2395c959730f024a5
Aeryn Caen <aeryncaen@gmail.com>
2026-01-29 23:00:55 -0500

    adsf

--- scripts/bench_ssm.py
+++ scripts/bench_ssm.py
@@ -305,7 +305,7 @@ def make_models(dim):
     mamba = StackedSSM(lambda: MambaWrapper(d_model=dim, d_state=64, d_conv=4, expand=1), n_layers=2)
 
     return {
-        'DS1': ds1,
+        # 'DS1': ds1,
         'DS1++': ds1_pp,
         # 'S4D': s4d,
         # 'S5': s5,
